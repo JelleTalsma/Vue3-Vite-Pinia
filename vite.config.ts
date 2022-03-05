8624e55daf12a6411daf25dfdc482280677aa88a
JelleTalsma <jelle@coddin.nl>
2022-03-05 17:42:45 +0100

    added .env support

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,41 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "env");
+
+  const htmlPlugin = () => {
+    return {
+      name: "html-transform",
+      transformIndexHtml(html: string) {
+        return html.replace(/%(.*?)%/g, function (match, p1) {
+          return env[p1];
+        });
+      },
+    };
+  };
+
+  return {
+    plugins: [vue(), htmlPlugin()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
           @import "./src/scss/global/_variables.scss";
           @import "./src/scss/global/_reset.scss";
           @import "./src/scss/global/_typography.scss";
           @import "./src/scss/global/_fonts.scss";
         `,
+        },
       },
     },
-  },
+  };
 });
